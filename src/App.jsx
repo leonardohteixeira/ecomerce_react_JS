17aaff307a9cb5e93bab11203ed3d7cf78f12dc1
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-04-04 15:47:11 -0300

    Adicionado estilo css na página de catálogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
-import './App.css'
 import Catalog from './components/Catalog'
 import Cart from './components/Cart'
 import ThankYouPage from './components/ThankYouPage'
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Product = ({product}) => {
   return (
-    <div className='product'>
-        <img src={product.image} alt={product.name} />
-        <h3>{product.title}</h3>
-        <p>${product.price}</p>
-        <div className="cart-buttons">
+    <div className="product">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>${product.price.toFixed(2)}</p>
+      <div className="cart-buttons">
             <select>
                 <option value="">1</option>
             </select>
